fe73c1c5106c70a2ec65212fe5b3ec857c03eac1
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2025-09-02 17:04:17 +0700

    add str_to_int to stlib.h

--- strlib.h
+++ strlib.h
@@ -4,5 +4,6 @@ bool str_starts_with(const char *string, const char *prefix);
 bool str_ends_with(const char *string, const char *suffix);
 void str_remove_all_whitespace_inplace(char *string);
 void str_trim_whitespace_inplace(char *string);
-void str_trim_leading_whitespace_inplace(char *str);
-void str_trim_trailing_whitespace_inplace(char *str);
+void str_trim_leading_whitespace_inplace(char *string);
+void str_trim_trailing_whitespace_inplace(char *string);
+int str_to_int(const char *string, int *out);
