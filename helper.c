75244eb0c860a7a5745ae4e9c0c56f90342c1e46
Izzuddin Ahmad Afif <izzuddinafif@gmail.com>
2026-02-09 14:03:15 +0700

    refactor: improve error handling and organize color constants
    Move terminal color constants from helper.c to helper.h for better code organization:
    - Relocate ANSI color codes to header file for shared access
    - Improve print_syntax_error formatting with better line breaks
    - Add get_color_for_fd function declaration to header
    - Comment out unused check_io_error function
    - Improve code formatting for better readability

--- helper.c
+++ helper.c
@@ -9,31 +9,9 @@
 #include <string.h>
 #include <unistd.h>
 
-// Text colors
-#define BLACK "\033[30m"
-#define RED "\033[31m"
-#define GREEN "\033[32m"
-#define YELLOW "\033[33m"
-#define BLUE "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN "\033[36m"
-#define WHITE "\033[37m"
-
-// Bright colors
-#define BRIGHT_RED "\033[91m"
-#define BRIGHT_GREEN "\033[92m"
-#define BRIGHT_YELLOW "\033[93m"
-
-// Background colors
-#define BG_RED "\033[41m"
-#define BG_GREEN "\033[42m"
-
-// Styles
-#define BOLD "\033[1m"
-#define UNDERLINE "\033[4m"
-#define RESET "\033[0m"
-
-const char *get_color_for_fd(int fd, const char *code) { return isatty(fd) ? code : ""; }
+const char *get_color_for_fd(int fd, const char *code) {
+  return isatty(fd) ? code : "";
+}
 
 Debugger debugger;
 Debugger *dbg = &debugger;
@@ -42,22 +20,23 @@ const int MAX_CONSTANT_SIZE = 32767;
 
 void init_debugger(Debugger *d, bool enabled) { d->enabled = enabled; }
 
-void check_io_error(FILE *file, const char *filename) {
-  if (ferror(file)) {
-    fprintf(stderr, "[ERROR] I/O error on %s: ", filename);
-    perror("");
-    fclose(file);
-    exit(1);
-  } else if (feof(file)) {
-    fprintf(stderr, "End of file reached: %s\n", filename);
-  }
-}
+// void check_io_error(FILE *file, const char *filename) {
+//   if (ferror(file)) {
+//     fprintf(stderr, "[ERROR] I/O error on %s: ", filename);
+//     perror("");
+//     fclose(file);
+//     exit(1);
+//   } else if (feof(file)) {
+//     fprintf(stderr, "End of file reached: %s\n", filename);
+//   }
+// }
 
 bool line_is_spaces_only_or_empty(const char *string) {
   if (!string)
     return true;
   while (*string) {
-    if (isspace((unsigned char)*string) == 0) { // cast to unsigned char to avoid UB
+    if (isspace((unsigned char)*string) ==
+        0) { // cast to unsigned char to avoid UB
       // printf("non space char found in %s\n", string);
       return false;
     }
@@ -73,7 +52,8 @@ void remove_comment_inplace(char *buffer) {
   }
 }
 
-void print_syntax_error(const char *line, const char *type, int line_number, int position, const char *format, ...) {
+void print_syntax_error(const char *line, const char *type, int line_number,
+                        int position, const char *format, ...) {
   va_list args;
   fputs(get_color_for_fd(fileno(stderr), RED), stderr);
   fprintf(stderr, "%*s^ ", position, "");
@@ -83,9 +63,10 @@ void print_syntax_error(const char *line, const char *type, int line_number, int
   vsnprintf(new_msg_buf, sizeof new_msg_buf, format, args);
   va_end(args);
 
-  fprintf(stderr, "%s[ERROR] Syntax error on line %d, column %d : %s in %s \"%s\"%s\n",
-          get_color_for_fd(fileno(stderr), RED), line_number, position + 1, new_msg_buf, type, line,
-          get_color_for_fd(fileno(stderr), RESET));
+  fprintf(stderr,
+          "%s[ERROR] Syntax error on line %d, column %d : %s in %s \"%s\"%s\n",
+          get_color_for_fd(fileno(stderr), RED), line_number, position + 1,
+          new_msg_buf, type, line, get_color_for_fd(fileno(stderr), RESET));
 }
 
 bool is_constant(const char *c) {
@@ -96,20 +77,21 @@ bool is_constant(const char *c) {
   return true;
 }
 
-// returns nullptr if valid or the invalid symbol's pointer otherwise
 const char *is_not_valid_symbol(char *symbol, InstructionType type) {
   bool is_constant_var = is_constant(symbol);
   if (!is_constant_var && isdigit((unsigned char)*symbol)) {
-    return symbol; // cant start with a digit
+    return symbol; // cant start symbol with a digit
   }
-  if (is_constant_var && (type == L_INSTRUCTION || !is_valid_const_size(symbol))) {
+  if (is_constant_var &&
+      (type == L_INSTRUCTION || !is_valid_const_size(symbol))) {
     return symbol;
   }
-  DEBUG_LOG(dbg, "checking string \'%s\'.. \n", symbol);
+  // DEBUG_LOG(dbg, "checking string \'%s\'.. ", symbol);
   while (*symbol) {
     int sym = (unsigned char)*symbol;
     // DEBUG_LOG(dbg, "checking char \'%c\'.. \n", *symbol);
-    if (!(isalnum(sym) || sym == '_' || sym == '.' || sym == '$' || sym == ':')) {
+    if (!(isalnum(sym) || sym == '_' || sym == '.' || sym == '$' ||
+          sym == ':')) {
       // DEBUG_LOG(dbg, "%c is NOT a valid symbol\n", sym);
       return symbol;
     }
@@ -139,8 +121,9 @@ const char *is_not_valid_c_instruction(const char *instruction) {
   // DEBUG_LOG(dbg, "checking C instruction \"%s\"\n", instruction);
   while (*instruction) {
     // DEBUG_LOG(dbg, "checking char \'%c\'\n", *instruction);
-    if (!(isalpha((unsigned char)(*instruction)) || *instruction == '1' || *instruction == '0' || *instruction == ';' ||
-          *instruction == '=' || *instruction == '-' || *instruction == '+' || *instruction == '!' ||
+    if (!(isalpha((unsigned char)(*instruction)) || *instruction == '1' ||
+          *instruction == '0' || *instruction == ';' || *instruction == '=' ||
+          *instruction == '-' || *instruction == '+' || *instruction == '!' ||
           *instruction == '&' || *instruction == '|')) {
       return instruction;
     }
@@ -160,18 +143,23 @@ const char *is_not_valid_c_instruction(const char *instruction) {
 }
 
 void reset_fields(Parser *parser, TranslatedCode *code) {
-  parser->currentInstruction[0] = '\0'; // set all string buffers to empty
-  parser->symbol[0] = '\0';
-  parser->jumpMnemonic[0] = '\0';
-  parser->compMnemonic[0] = '\0';
-  parser->destMnemonic[0] = '\0';
-  parser->typeString[0] = '\0';
-  parser->type = NO_INSTRUCTION;
-  parser->errorStatus = false;
+  if (parser) {
+    parser->currentInstruction[0] = '\0'; // set all string buffers to empty
+    parser->symbol[0] = '\0';
+    parser->jumpMnemonic[0] = '\0';
+    parser->compMnemonic[0] = '\0';
+    parser->destMnemonic[0] = '\0';
+    parser->typeString[0] = '\0';
+    parser->type = NO_INSTRUCTION;
+    parser->errorStatus = false;
+    parser->instructionAddress = 0;
+  }
 
-  code->comp[0] = '\0';
-  code->dest[0] = '\0';
-  code->jump[0] = '\0';
+  if (code) {
+    code->comp[0] = '\0';
+    code->dest[0] = '\0';
+    code->jump[0] = '\0';
+  }
 }
 
 void clean_output(Writer *writer) { writer->output[0] = '\0'; }
@@ -196,4 +184,28 @@ void int_str_to_bit_str(const char *in, char *bit, size_t buf_size) {
     // DEBUG_LOG(dbg, "result %d remainder %d\n", result, remainder);
   }
   bit[buf_size - 1] = '\0';
+}
+
+bool int_to_str(int in, char *buf, size_t buf_size) {
+  if (!buf || buf_size < 2) {
+    return false;
+  }
+  char reversed[S32];
+  int digit = 0;
+  int i = 0;
+
+  while (in > 0) {
+    digit = in % 10;
+    reversed[i++] = (char)digit + 48;
+    in /= 10;
+  }
+  reversed[i] = '\0';
+
+  int j = i - 1;
+  int k = 0;
+  while (j >= 0) {
+    buf[k++] = reversed[j--];
+  }
+  buf[k] = '\0';
+  return true;
 }
--- helper.h
+++ helper.h
@@ -5,6 +5,32 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// Text colors
+#define BLACK "\033[30m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN "\033[36m"
+#define WHITE "\033[37m"
+
+// Bright colors
+#define BRIGHT_RED "\033[91m"
+#define BRIGHT_GREEN "\033[92m"
+#define BRIGHT_YELLOW "\033[93m"
+
+// Background colors
+#define BG_RED "\033[41m"
+#define BG_GREEN "\033[42m"
+
+// Styles
+#define BOLD "\033[1m"
+#define UNDERLINE "\033[4m"
+#define RESET "\033[0m"
+
+const char *get_color_for_fd(int fd, const char *code);
+
 #ifdef _WIN32
 #include <windows.h>
 #else
@@ -16,7 +42,8 @@ typedef struct {
 } Debugger;
 
 #if defined(__GNUC__) || defined(__clang__)
-#define PRINTF_LIKE(fmt_idx, first_arg) __attribute__((format(printf, fmt_idx, first_arg)))
+#define PRINTF_LIKE(fmt_idx, first_arg)                                        \
+  __attribute__((format(printf, fmt_idx, first_arg)))
 #else
 #define PRINTF_LIKE(fmt_idx, first_arg)
 #endif
@@ -26,8 +53,8 @@ static inline void debug_get_time(char *buf, size_t size) {
   SYSTEMTIME st;
   GetLocalTime(&st);
 
-  snprintf(buf, size, "%04d-%02d-%02d %02d:%02d:%02d.%03d", st.wYear, st.wMonth, st.wDay, st.wHour, st.wMinute,
-           st.wSecond, st.wMilliseconds);
+  snprintf(buf, size, "%04d-%02d-%02d %02d:%02d:%02d.%03d", st.wYear, st.wMonth,
+           st.wDay, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds);
 #else
   struct timespec ts;
   struct tm tm;
@@ -36,37 +63,48 @@ static inline void debug_get_time(char *buf, size_t size) {
   localtime_r(&ts.tv_sec, &tm);
 
   strftime(buf, size, "%Y-%m-%d %H:%M:%S", &tm);
-  snprintf(buf + strlen(buf), size - strlen(buf), ".%03ld", ts.tv_nsec / 1000000);
+  snprintf(buf + strlen(buf), size - strlen(buf), ".%03ld",
+           ts.tv_nsec / 1000000);
 #endif
 }
 
+// #define DEBUG
 #ifdef DEBUG
 
-static void debug_vprint(const char *file, int line, const char *func, const char *fmt, va_list args) PRINTF_LIKE(4, 0);
+static void debug_vprint(const char *file, int line, const char *func,
+                         const char *fmt, va_list args) PRINTF_LIKE(4, 0)
+    __attribute__((unused));
 
-static void debug_print(const char *file, int line, const char *func, const char *fmt, ...) PRINTF_LIKE(4, 5);
+static void debug_print(const char *file, int line, const char *func,
+                        const char *fmt, ...) PRINTF_LIKE(4, 5)
+    __attribute__((unused));
 
-static void debug_vprint(const char *file, int line, const char *func, const char *fmt, va_list args) {
+static void debug_vprint(const char *file, int line, const char *func,
+                         const char *fmt, va_list args) {
   char timebuf[64];
   debug_get_time(timebuf, sizeof(timebuf));
 
-  fprintf(stderr, "[DEBUG] %s | %s:%d (%s): ", timebuf, file, line, func);
+  fprintf(stderr, "%s[DEBUG] %s | %s:%d (%s): %s",
+          get_color_for_fd(fileno(stdout), BLUE), timebuf, file, line, func,
+          get_color_for_fd(fileno(stdout), RESET));
 
   vfprintf(stderr, fmt, args);
   fputc('\n', stderr);
 }
 
-static void debug_print(const char *file, int line, const char *func, const char *fmt, ...) {
+static void debug_print(const char *file, int line, const char *func,
+                        const char *fmt, ...) {
   va_list args;
   va_start(args, fmt);
   debug_vprint(file, line, func, fmt, args);
   va_end(args);
 }
 
-#define DEBUG_LOG(dbg, fmt, ...)                                                                                       \
-  do {                                                                                                                 \
-    if ((dbg) && (dbg)->enabled)                                                                                       \
-      debug_print(__FILE__, __LINE__, __func__, fmt __VA_OPT__(, ) __VA_ARGS__);                                       \
+#define DEBUG_LOG(dbg, fmt, ...)                                               \
+  do {                                                                         \
+    if ((dbg) && (dbg)->enabled)                                               \
+      debug_print(__FILE__, __LINE__, __func__,                                \
+                  fmt __VA_OPT__(, ) __VA_ARGS__);                             \
   } while (0)
 
 #else
@@ -80,10 +118,10 @@ static void debug_print(const char *file, int line, const char *func, const char
 //
 // NOT like this:
 // FREE(array[i++]); // !DON'T DO THIS!
-#define FREE(p)                                                                                                        \
-  do {                                                                                                                 \
-    free(p);                                                                                                           \
-    (p) = nullptr;                                                                                                     \
+#define FREE(p)                                                                \
+  do {                                                                         \
+    free(p);                                                                   \
+    (p) = nullptr;                                                             \
   } while (0)
 
 bool is_constant(const char *c);
@@ -94,11 +132,13 @@ void init_debugger(Debugger *debugger, bool enabled);
 void check_io_error(FILE *file, const char *filename);
 bool line_is_spaces_only_or_empty(const char *string);
 void remove_comment_inplace(char *buffer);
-void print_syntax_error(const char *line, const char *type, int line_number, int position, const char *format, ...)
+void print_syntax_error(const char *line, const char *type, int line_number,
+                        int position, const char *format, ...)
     __attribute__((format(printf, 5, 6)));
 void reset_fields(Parser *parser, TranslatedCode *code);
 void clean_output(Writer *writer);
 void int_str_to_bit_str(const char *in, char *bit, size_t buf_size);
+bool int_to_str(int in, char *buf, size_t buf_size);
 
 extern Debugger debugger;
 extern Debugger *dbg;
